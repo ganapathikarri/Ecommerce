260954d3a151b5048be8230b8a80090219d73f8f
ganapathikarri <131351112+ganapathikarri@users.noreply.github.com>
2026-04-28 14:00:20 +0530

    Delete src directory

--- src/components/Spinner.tsx
+++
@@ -1,8 +0,0 @@
-import { Loader2 } from "lucide-react";
-import { cn } from "@/lib/utils";
-
-const Spinner = ({ className }: { className?: string }) => (
-  <Loader2 className={cn("h-5 w-5 animate-spin text-primary", className)} />
-);
-
-export default Spinner;
